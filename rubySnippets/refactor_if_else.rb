6a7b601d5ea238fe07a023475cee77dd40c71eca
Graham Hewett <hewettgraham@gmail.com>
2018-08-21 14:03:55 +0100

    remove ruby files

--- rubySnippets/refactor_if_else.rb
+++
@@ -1,7 +0,0 @@
-6 < 5 ? state = "That doesn't make sense" : state = "I agree that is false"
-puts state
-
-//rint only even numbers from an array
-
-my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-my_array.each {|num| puts num%2==0 ? num : nil}
